210fd5224e4515029328f7e543a54e0df98860eb
MadPepe <mateuszstolowski95@gmail.com>
2021-04-18 17:53:55 +0200

    add database manager

--- managers.py
+++ managers.py
@@ -1,6 +1,24 @@
-import sqllite3
+import sqlite3
+from os.path import exists as path_exists
+from os import mkdir
 
 
 class DatabaseManger:
-    def __init__(self, file_name):
-        self.connection = sqlite3.connect(file_name)
+    def __init__(self, file_path="Data"):
+        self.database_directory = self._get_or_create_relative_directory(file_path)
+        self.connection = sqlite3.connect(self.database_directory)
+
+    def __del__(self):
+        self.connection.close()
+
+    def _execute(self, query, values=None):
+        with self.connection:
+            cursor = self.connection.cursor()
+            cursor.execute(query, values or [])
+            return cursor
+
+    @staticmethod
+    def _get_or_create_relative_directory(directory):
+        if not path_exists(directory):
+            mkdir(directory)
+        return f"{directory}/database.db"
